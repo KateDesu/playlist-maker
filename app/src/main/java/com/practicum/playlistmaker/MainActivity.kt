0b1653b5c8d20e912d7b4c9f68fdb7cb88a17e9a
Екатерина Петрова <katedesu1@yandex.ru>
2025-03-14 18:50:12 +0500

    Спринт 8 ДЗ 2 Реализованы нажатия на кнопки главного экрана с заглушками в виде Toast

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+
+        // реализация клика через анонимный класс
+        val btnSearch=findViewById<Button>(R.id.btn_search)
+        
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Нажатие на Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
+        // реализация клика через лямбда-выражение
+        val btnLibrary = findViewById<Button>(R.id.btn_library)
+
+        btnLibrary.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажатие на Медиатеку", Toast.LENGTH_SHORT).show()
+        }
+
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажатие на Настройки", Toast.LENGTH_SHORT).show()
         }
     }
 }
