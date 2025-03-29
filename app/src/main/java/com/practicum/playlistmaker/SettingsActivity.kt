4bec1c55c481d37af047cec3df1cfc68f0c67a8b
Екатерина Петрова <katedesu1@yandex.ru>
2025-03-29 15:00:20 +0500

    Спринт 9 ДЗ2 Добавлены обработчики кликов TextView для страницы Settings

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,46 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareTextView = findViewById<TextView>(R.id.text_view_share_app)
+        val urlPracticum = Uri.parse(getString(R.string.url_practicum))
+
+        shareTextView.setOnClickListener{
+            val intent=Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_TEXT, urlPracticum)
+            startActivity(intent)
+        }
+
+        val writeToSupportTextView = findViewById<TextView>(R.id.text_view_write_to_support)
+
+        writeToSupportTextView.setOnClickListener {
+            val messageHeader = getString(R.string.message_header)
+            val messageBody=getString(R.string.message_body)
+
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_SUBJECT, messageHeader)
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_author)))
+            intent.putExtra(Intent.EXTRA_TEXT, messageBody)
+            startActivity(intent)
+        }
+
+        val userAgreementTextView = findViewById<TextView>(R.id.text_view_user_agreement)
+        val urlAgreement = Uri.parse(getString(R.string.url_agreement))
+
+        userAgreementTextView.setOnClickListener{
+            val intent=Intent(Intent.ACTION_VIEW, urlAgreement)
+            startActivity(intent)
+        }
     }
 }
