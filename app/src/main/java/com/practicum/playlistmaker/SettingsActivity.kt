e436788f61e0dc81b5d183c8fb7b2fea76f19d79
Екатерина Петрова <katedesu1@yandex.ru>
2025-04-06 17:00:48 +0500

    Спринт 9 ДЗ3 Сверстан экран Поиска.  Добавлены цвета для темной темы. Цвета иконок вынесены в ресурсы

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,14 +3,23 @@ package com.practicum.playlistmaker
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val shareTextView = findViewById<TextView>(R.id.text_view_share_app)
         val urlPracticum = Uri.parse(getString(R.string.url_practicum))
