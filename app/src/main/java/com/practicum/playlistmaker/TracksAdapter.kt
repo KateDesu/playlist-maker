eb9685a82b82da6dc46b02860d9d3b193ee28908
Екатерина Петрова <katedesu1@yandex.ru>
2025-04-25 00:00:38 +0500

    Спринт 11 ДЗ Отображены треки с помощью iTunes Search, выполнен поиск по ним с плейсхолдерами

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -4,13 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TracksAdapter (
-    private val tracks: List<Track>
-) : RecyclerView.Adapter<TracksViewHolder> () {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder {
-        val view=LayoutInflater.from(parent.context).inflate(R.layout.item_view_track,parent,false)
-        return TracksViewHolder(view)
-    }
+class TracksAdapter(
+//    private val tracks: List<Track>
+) : RecyclerView.Adapter<TracksViewHolder>() {
+    var tracks = ArrayList<Track>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TracksViewHolder = TracksViewHolder(parent)
+//    {
+//        val view =
+//            LayoutInflater.from(parent.context).inflate(R.layout.item_view_track, parent, false)
+//        return TracksViewHolder(view)
+//    }
 
     override fun onBindViewHolder(holder: TracksViewHolder, position: Int) {
         holder.bind(tracks[position])
--- app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksViewHolder.kt
@@ -1,14 +1,24 @@
 package com.practicum.playlistmaker
 
-import android.view.RoundedCorner
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.view.ViewParent
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import java.text.SimpleDateFormat
+import java.util.Locale
 
-class TracksViewHolder(trackView: View) : RecyclerView.ViewHolder(trackView) {
+//class TracksViewHolder(trackView: View) :
+//    RecyclerView.ViewHolder(trackView) {
+class TracksViewHolder(parent: ViewGroup) :
+        RecyclerView.ViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_view_track, parent, false)) {
+
     private val trackName: TextView = itemView.findViewById(R.id.textview_track_name)
     private val artistName: TextView = itemView.findViewById(R.id.textview_artist_name)
     private val trackTime: TextView = itemView.findViewById(R.id.textview_track_time)
@@ -17,7 +27,9 @@ class TracksViewHolder(trackView: View) : RecyclerView.ViewHolder(trackView) {
     fun bind(model: Track) {
         trackName.text = model.trackName
         artistName.text = model.artistName
-        trackTime.text = model.trackNTime
+
+        trackTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(model.trackTime.toLong())
+        Log.d("trackTime", trackTime.text.toString())
 
         Glide.with(itemView.context)
             .load(model.artworkUrl100)
