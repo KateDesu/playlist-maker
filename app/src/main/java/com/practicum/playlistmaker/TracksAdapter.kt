429762512b3bccfd94cc589db2ebea5216a15f3e
Екатерина Петрова <katedesu1@yandex.ru>
2025-05-28 15:36:03 +0500

    Спринт 14 Использование debounce для предотвращения двойного нажатия на трек в SearchActivity

--- app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
+++ app/src/main/java/com/practicum/playlistmaker/TracksAdapter.kt
@@ -2,6 +2,8 @@ package com.practicum.playlistmaker
 
 import android.app.Application.MODE_PRIVATE
 import android.content.Intent
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
@@ -11,6 +13,23 @@ import com.google.gson.Gson
 class TracksAdapter(
 ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     var tracks = ArrayList<Track>()
+
+    companion object {
+        private const val CLICK_DEBOUNCE_DELAY = 1000L
+    }
+
+    private var isClickAllowed = true
+
+    private val handler = Handler(Looper.getMainLooper())
+
+    private fun clickDebounce() : Boolean {
+        val current = isClickAllowed
+        if (isClickAllowed) {
+            isClickAllowed = false
+            handler.postDelayed({ isClickAllowed = true }, CLICK_DEBOUNCE_DELAY)
+        }
+        return current
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return TracksViewHolder(
@@ -34,10 +53,12 @@ class TracksAdapter(
             val gson = Gson()
             val trackJsonString: String = gson.toJson(track)
 
-            val trackIntent = Intent(holder.itemView.context, TrackActivity::class.java)
-            trackIntent.putExtra("trackJson", trackJsonString)
-            Log.d("trackJson", trackJsonString)
-            holder.itemView.context.startActivity(trackIntent)
+            if (clickDebounce()) {
+                val trackIntent = Intent(holder.itemView.context, TrackActivity::class.java)
+
+                trackIntent.putExtra("trackJson", trackJsonString)
+                holder.itemView.context.startActivity(trackIntent)
+            }
         }
     }
 
