ea0e94a4388bc2c1d5b90e247b595a21e70d85ad
Екатерина Петрова <katedesu1@yandex.ru>
2025-07-30 00:12:10 +0500

    Спринт 16 Использование Binding в MainActivity

--- app/src/main/java/com/practicum/playlistmaker/ui/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/ui/MainActivity.kt
@@ -3,51 +3,47 @@ package com.practicum.playlistmaker.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.ActivityMainBinding
 import com.practicum.playlistmaker.ui.library.LibraryActivity
 import com.practicum.playlistmaker.ui.search.SearchActivity
 import com.practicum.playlistmaker.ui.settings.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
         // реализация клика через анонимный класс
-        val searchButton = findViewById<Button>(R.id.btn_search)
-
         val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
             override fun onClick(v: View?) {
                 val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
                 startActivity(searchIntent)
             }
         }
-
-        searchButton.setOnClickListener(searchButtonClickListener)
+        binding.btnSearch.setOnClickListener(searchButtonClickListener)
 
         // реализация клика через лямбда-выражение
-        val libraryButton = findViewById<Button>(R.id.btn_library)
-
-        libraryButton.setOnClickListener {
+        binding.btnLibrary.setOnClickListener {
             val libraryIntent = Intent(this, LibraryActivity::class.java)
             startActivity(libraryIntent)
 
         }
 
-        val settingsButton = findViewById<Button>(R.id.btn_settings)
-
-        settingsButton.setOnClickListener {
+        binding.btnSettings.setOnClickListener {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
         }
